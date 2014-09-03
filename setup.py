690021fd11ee0d13223dbf9b66405db4b7c45778
R Zach Lamberty <r.zach.lamberty@gmail.com>
2014-09-03 06:39:53 -0500

    version 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='zachlog',
-    version='1.0.0',
+    version='1.0.1',
     description='Ripped off logging library from peak6',
     include_package_data=True,
     author='Zach Lamberty',
